d57ecff7bac13f46a08d7e7d15de2d1a89e47eb9
Abdelrahman Zayed <50181300+AbdelrahmanZayed@users.noreply.github.com>
2021-06-27 20:50:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import argparse
 from experiment import run_experiment
+from classifier import measure_bias_metrics
 
 parser = argparse.ArgumentParser()
 
@@ -13,11 +14,24 @@ parser.add_argument('--num_epochs', type=int, default=15,
                     help='Number of training epochs for the policy gradient algorithm')
 parser.add_argument('--learning_rate', type=float, default=1.41e-4,
                     help='learning rate for the Bert classifier')
-
+parser.add_argument('--PG_lambda', type=float, default=0.01,
+                    help='The hyperparameter controling the rewards due to bias and that due to miscalssification')
 #arguments for the classifier
 parser.add_argument('--classifier_model', choices=['bert-base-uncased'],
                     default='bert-base-uncased', help='Type of classifier used')
 parser.add_argument('--dataset', choices=['Equity-Evaluation-Corpus,twitter_dataset'],
                     default='twitter_dataset', help='Type of dataset used')
-parser.add_argument('--num_epochs_classifier', type=int, default=3,
+parser.add_argument('--num_epochs_classifier', type=int, default=1,
                     help='Number of training epochs for the classifier')
+parser.add_argument('--batch_size_classifier', type=int, default=32,
+                    help='Batch size for the classifier')
+parser.add_argument('--load_pretrained_classifier', type=bool, default=False,
+                    help='Whether or load a pretrained classifier')
+
+def main(args):
+    model,tokenizer=run_experiment(args)
+    measure_bias_metrics(model,tokenizer,args)
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    main(args)
